c83e708693243ccebfe06e801cdc938d32c1d756
offers-png <offers@dealdily.com>
2025-10-24 22:15:42 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from "./Clipper.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function ErrorBoundary({ children }) {
+  try {
+    return children;
+  } catch (err) {
+    console.error("❌ App crashed:", err);
+    return <div style={{ color: "red", textAlign: "center" }}>App crashed: {err.message}</div>;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
