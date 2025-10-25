89243655a06ca36132db6f79f74309f47abb235f
offers-png <offers@dealdily.com>
2025-10-24 21:22:54 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,27 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+export default defineConfig({
+  ignorePatterns: ['dist'], // simpler and standard way
+  files: ['**/*.{js,jsx}'],
+  extends: [
+    js.configs.recommended,
+    reactHooks.configs['recommended-latest'],
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      ecmaFeatures: { jsx: true },
+      sourceType: 'module',
     },
   },
-])
+  rules: {
+    'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+    'react/react-in-jsx-scope': 'off', // React 17+ doesn't need import React
+  },
+})
