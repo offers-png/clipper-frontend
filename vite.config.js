b354d63365646225c67810148a8c2a24088ed4b5
offers-png <offers@dealdily.com>
2025-10-24 22:20:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // make sure root is project root
+  build: {
+    outDir: 'dist', // where Render will deploy from
+  },
   server: {
     port: 5173,
   },
