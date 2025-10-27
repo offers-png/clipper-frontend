56d2294cbe412208ed62b47a8d222fcc02f46b95
offers-png <offers@dealdily.com>
2025-10-26 21:00:17 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
+
 const url = import.meta.env.VITE_SUPABASE_URL;
 const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
