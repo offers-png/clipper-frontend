d0d34a075199871008f980f9bd7f73ed2a8f5504
offers-png <offers@dealdily.com>
2025-10-26 20:19:38 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthForm from "./AuthForm";
-import Clipper from "./Clipper";
-import Dashboard from "./Dashboard";
+import Clipper from "./Clipper.jsx";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthForm />} />
-        <Route path="/clipper" element={<Clipper />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  return <Clipper />;
 }
-
-export default App;
