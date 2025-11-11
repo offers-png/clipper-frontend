d81acee2ab60b599e48f618ad4c8a73295b23c13
offers-png <offers@dealdily.com>
2025-11-10 20:59:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Clipper from "./Clipper";
-import Login from "./Login";
-import ProtectedRoute from "./ProtectedRoute";
+import Clipper from "./Clipper.jsx";
+import Login from "./Login.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
 export default function App() {
   return (
