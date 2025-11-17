2fbafda8ac60848da86399b085d4be28e6e018ea
offers-png <offers@dealdily.com>
2025-11-17 17:51:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-// src/App.jsx
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
 import Clipper from "./Clipper";
 import ProtectedRoute from "./ProtectedRoute";
-import LoginPage from "./Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Route */}
-        <Route path="/" element={<AuthForm />} />
+        <Route path="/" element={<LoginPage />} />
 
-        {/* Protected Route */}
         <Route
           path="/app"
           element={
